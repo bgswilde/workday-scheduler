936718c070f58a0429ce97d46b424d6c7d03c7b2
Brian <bgswilde@gmail.com>
2021-07-25 17:26:21 -0500

    working code for time colors and task persistency

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,67 @@
 // Sets the current day at the top of the page
 var currentDay = moment().format("LL");
 $("#currentDay").html(currentDay);
+// Define functions to load time and set time colors
+
+var loadTask = function() {
+    // get tasks from local storage (runs for each line)
+    $("#task-hour8").val(localStorage.getItem("8"));
+    $("#task-hour9").val(localStorage.getItem("9"));
+    $("#task-hour10").val(localStorage.getItem("10"));
+    $("#task-hour11").val(localStorage.getItem("11"));
+    $("#task-hour12").val(localStorage.getItem("12"));
+    $("#task-hour13").val(localStorage.getItem("13"));
+    $("#task-hour14").val(localStorage.getItem("14"));
+    $("#task-hour15").val(localStorage.getItem("15"));
+    $("#task-hour16").val(localStorage.getItem("16"));
+    $("#task-hour17").val(localStorage.getItem("17"));
+};
 
 // function to set variables to record in local storage, defining time and tasks.
-var saveTasks = function() {
-    // establish the data to be saved
-    var task = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
+$(document).ready(function() {
+    // saveBtn function on click
+    $(".saveBtn").on("click", function() {
+        // establish the data to be saved
+        var task = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
-    // save the task data to localStorage
-    localStorage.setItem(time, task);
-    console.log(localStorage);
-};
-
-// run saveTasks when the save button on corresponding row is clicked
-$(".saveBtn").on("click", saveTasks())
-
-// button highlights green when 
-// get tasks from local storage (runs for each line)
-$(document).on("load", function() {
-    $("#8AM .description").val(localStorage.getItem("8AM"));
-    $("#9AM .description").val(localStorage.getItem("9AM"));
-    /*$("#10AM .description").val(localStorage.getItem("10AM"));
-    $("#11AM .description").val(localStorage.getItem("11AM"));
-    $("#12PM .description").val(localStorage.getItem("12PM"));
-    $("#1PM .description").val(localStorage.getItem("1PM"));
-    $("#2PM .description").val(localStorage.getItem("2PM"));
-    $("#3PM .description").val(localStorage.getItem("3PM"));
-    $("#4PM .description").val(localStorage.getItem("4PM")); */
-});
-
-
-// function to set hour block colors based on current time
-var identifyTime = function () {
-    // get the hour of the current time
-    var currentTime = moment().hour();
-
-    // check time relative to each time block
-    $(".time-block").each(function () {
-        var taskTime = parseInt($(this)
-            .attr("id")
-            .trim()
-            [1]
-            );
+        // save the task data to localStorage
+        localStorage.setItem(time, task);
         
-        // To check the time and add the classes for background indicators
-        if (taskTime === currentTime) {
-            $(this).removeClass("past");
-            $(this).removeClass("future");
-            $(this).addClass("present");
-        }
-        if (taskTime < currentTime) {
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-            $(this).addClass("past");
-        }
-        if (taskTime > currentTime) {
-            $(this).removeClass("past");
-            $(this).removeClass("present");
-            $(this).addClass("future");
-        }
+        // give a confirmation message for 3 seconds
+        $("#save-message").text("Your task has been saved");
+        setTimeout(function(){
+            $("#save-message").empty();
+            }, 2000);
     })
-};
 
-identifyTime();
+    var identifyTime = function() {
+        // get the hour of the current time
+        var currentTime = moment().hour();
+    
+        // check time relative to each time block
+        $(".description").each(function () {
+            var taskTime = parseInt($(this).attr("id").split("task-hour")[1]);
+            
+            // add the css class that makes the color display based on past, present or future
+            if (taskTime === currentTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            if (taskTime < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            if (taskTime > currentTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        })
+    };
+
+    identifyTime();
+    loadTask();
+});
