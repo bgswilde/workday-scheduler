65b02c00cf96c9ee87746f67c140d6ba281f1e8a
Brian <bgswilde@gmail.com>
2021-07-24 19:52:53 -0500

    file structure and pseudocode logic in html and js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Sets the current day at the top of the page
+var currentDay = moment().format("MMMM Do, YYYY");
+$("#currentDay").html(currentDay);
+
+// function to set variables to record in local storage, defining time and tasks
+
+// function to set hour block colors based on current time
+
+// functions to get items from local storage
